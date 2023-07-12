d51cce2a0a7431c30af8272a7abe215d14c3e0cf
Keyhole-Koro <130579219+Keyhole-Koro@users.noreply.github.com>
2023-07-13 06:44:01 +0900

    temporary save

--- demonstrate.py
+++ demonstrate.py
@@ -2,49 +2,79 @@ import time
 import array
 import datetime
 
-previous_y_value = 0
+count = 0
 
 def main(file):
 	data = file.read().split()
 	l_input_data = []
 	input_data = array.array('i')
 	count = 0
-	start_time = datetime.datetime()
+	start_time = time.time()
 	function1Interval = 20
 	function2Interval = 1000
 	
-	function1PreviousMillis = convert_millis(current_time)
-	function2PreviousMillis = convert_millis(current_time)
+	stored_values = {'current_inclination':0,'previous_inclination':0}
+	
+	function1PreviousMillis = convert_millis(start_time)
+	function2PreviousMillis = convert_millis(start_time)
 	#equivalent to loop
-	for d in data:
+	index = 0
+	while True:
+		d = data[index]
 		#in milliseconds which is equivalent to millis in arduino
 		elapsed_time = elapsed(start_time)
-		
+		print(index)
+		print(function1PreviousMillis, elapsed_time)
 		#get values from ldr
-		if elapsed_time - function1PreviousMillis  >= function1Interval:
-			current_time = datetime.datetime()
-			function1PreviousMillis = convert_millis(current_time)
+		if function1PreviousMillis - elapsed_time >= function1Interval:
+			seconds = time.time()
+			function1PreviousMillis = convert_millis(seconds)
+			
+			previous_ele_num = max(0, count-5)#it doesnt include [0-4] i think
+			
+			new_value = int(d)
+			
 			if previous_ele_num == 0:
-				previous_y_value = d
-			set_value(d, previous_y_value)
+				previous_value = 0
+				new_inclination = int(d)
+			else:
+				print(new_value, previous_value)
+				new_inclination = inclination(2, new_value - previous_value)
+				
+			stored_values['current_inclination'] = new_inclination
 			
-		if elapsed_time - function2PreviousMillis  >= function2Interval:#calculus
-			current_time = datetime.datetime()
-			function2PreviousMillis = convert_millis(current_time)
-			smooth_data(input_data, 5)
+			mean_value = int(smooth_data(stored_values))
+			print(mean_value)
 			
-def set_value(new_y_value, previous_y_value):
-	x_range = 2
-	y_range = new_y_value - previous_y_value
-	inclination_value = int(inclination(x_range, y_range))
-	input_data.append(inclination_value)
+			input_data.append(mean_value)
+			if count == 200:
+				l_input_data.append(input_data)
+				del input_data
+				input_data = array.array('i')
+				count = 0
+			count++1
+			
+			previous_inclination = mean_value
+			
+			function2PreviousMillis = convert_millis(seconds)
+			
+			index++1
+
+		if function2PreviousMillis - elapsed_time >= function2Interval:#calculus
+			seconds2 = time.time()
+			
+		time.sleep(0.01)
+			
+			
+def set_value(input_data, value):
+	input_data.append(value)
 	if count == 200:#200/2
 		l_input_data.append(input_data)
 		del input_data
 		input_data = array.array('i')
 		count = 0
-	previous_y_value = new_y_value
 	count++1
+	return input_data
 	
 def inclination(x_range, y_range):
 	value = y_range/x_range
@@ -53,39 +83,32 @@ def inclination(x_range, y_range):
 def set_mean_values():
 	pass
 
-def mean_values(near_sum, new_value, previous_value):
-	difference = new_value - previous_value
-	new_sum = near_sum + difference
+def mean_values(new_value, previous_value):
+	new_sum = new_value + previous_value
 	near_sum = new_sum/2
 	return near_sum
 	
 	
-def elapsed(time):
-	between = datetime.datetime()-time
-	millis = convert_milli(between)
+def elapsed(start_time):
+	now = time.time()
+	now_milli = convert_millis(now)
+	start_milli = convert_millis(start_time)
+	between_milli = now_milli - start_milli
+	#print(now_milli, start_milli, between_milli)
+	millis = between_milli
 	return millis
 
-def convert_millis(time):
-	seconds = time.total_seconds()
-	millis = round(seconds*1000)
-	return millis
+def convert_millis(seconds):
+	milliseconds = round(seconds*1000)
+	return milliseconds
 
-def smooth_data(data, window_size):
-	cumulative_sum = [0] * len(data)
-	smoothed_data = [0] * len(data)
-
-	cumulative_sum[0] = data[0]
-	for i in range(1, len(data)):
-		cumulative_sum[i] = cumulative_sum[i-1] + data[i]
-
-	half_window = window_size // 2
-	for i in range(len(data)):
-		start_index = max(0, i - half_window)
-		end_index = min(len(data) - 1, i + half_window)
-		window_size_actual = end_index - start_index + 1
-		smoothed_data[i] = int((cumulative_sum[end_index] - cumulative_sum[start_index] + data[start_index]) / window_size_actual)
-		
-	return smoothed_data
+def smooth_data(stored_values):
+	new_value = stored_values['current_inclination']
+	previous_value = stored_values['previous_inclination']
+	
+	mean_value = mean_values(new_value, previous_value)
+	
+	return mean_value
 
 def makeplot(x, y):
 	plt.plot(x, y)
