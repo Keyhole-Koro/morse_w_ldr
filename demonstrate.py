9f4ea80ea68932e0d5f166ef2e02fd4622963ac7
Keyhole-Koro <130579219+Keyhole-Koro@users.noreply.github.com>
2023-07-13 07:47:55 +0900

    func1 is called 47/50 per 1 sec

--- demonstrate.py
+++ demonstrate.py
@@ -1,81 +1,88 @@
 import time
 import array
 import datetime
+import threading#is not implemented in arduino
 
-count = 0
-
-def main(file):
-	data = file.read().split()
-	l_input_data = []
-	input_data = array.array('i')
-	count = 0
-	start_time = time.time()
-	function1Interval = 20
-	function2Interval = 1000
-	
-	stored_values = {'current_inclination':0,'previous_inclination':0}
-	
-	function1PreviousMillis = convert_millis(start_time)
-	function2PreviousMillis = convert_millis(start_time)
-	#equivalent to loop
-	index = 0
-	while True:
-		d = data[index]
-		#in milliseconds which is equivalent to millis in arduino
-		elapsed_time = elapsed(start_time)
-		print(index)
-		print(function1PreviousMillis, elapsed_time)
-		#get values from ldr
-		if function1PreviousMillis - elapsed_time >= function1Interval:
-			seconds = time.time()
-			function1PreviousMillis = convert_millis(seconds)
+class morse:
+	"""value is single
+	data is multiple like list array
+	(in most cases)"""
+	def __init__(self, file):
+		self.file = file
+		self.data = self.file.read().split()
+		
+		self.l_input_data = []
+		self.count = 0
+		self.input_data = array.array('i')
+		self.index = 0
+		self.mean_data = array.array('i')
+		self.mean_value_count = 0
+		
+	def main(self):
+		start_time = time.time()
+		function1Interval = 20
+		function2Interval = 1000
+		
+		function1PreviousMillis = 0
+		function2PreviousMillis = 0
+		
+		while True:
+			#in milliseconds which is equivalent to millis in arduino
+			elapsed_time = elapsed(start_time)
 			
-			previous_ele_num = max(0, count-5)#it doesnt include [0-4] i think
-			
-			new_value = int(d)
-			
-			if previous_ele_num == 0:
-				previous_value = 0
-				new_inclination = int(d)
-			else:
-				print(new_value, previous_value)
-				new_inclination = inclination(2, new_value - previous_value)
+			if elapsed_time - function1PreviousMillis >= function1Interval:
+				new_value = int(self.data[self.index])#get values from ldr
 				
-			stored_values['current_inclination'] = new_inclination
-			
-			mean_value = int(smooth_data(stored_values))
-			print(mean_value)
-			
-			input_data.append(mean_value)
-			if count == 200:
-				l_input_data.append(input_data)
-				del input_data
-				input_data = array.array('i')
-				count = 0
-			count++1
-			
-			previous_inclination = mean_value
-			
-			function2PreviousMillis = convert_millis(seconds)
-			
-			index++1
+				self.set_value(new_value)
 
-		if function2PreviousMillis - elapsed_time >= function2Interval:#calculus
-			seconds2 = time.time()
-			
-		time.sleep(0.01)
-			
-			
-def set_value(input_data, value):
-	input_data.append(value)
-	if count == 200:#200/2
-		l_input_data.append(input_data)
-		del input_data
-		input_data = array.array('i')
-		count = 0
-	count++1
-	return input_data
+				smoothed_value = self.smooth_value(new_value)
+				
+				self.set_mean(smoothed_value)
+				
+				print(self.mean_value_count, smoothed_value)
+				self.isFull()
+				
+				seconds = time.time()
+				function1PreviousMillis = elapsed_time
+
+			if elapsed_time - function2PreviousMillis >= function2Interval:#calculus
+				print('called2')
+				function2PreviousMillis = elapsed_time
+
+
+	def smooth_value(self, new_value):
+		
+		#self.index = len(self.input_data)#might take much time
+		self.index = self.count
+		start_index = max(0, self.index-5)
+		end_index = self.index
+		
+		clipped_data = self.input_data[start_index:end_index]
+		
+		mean_value = sum(clipped_data)/len(clipped_data)
+
+		return int(mean_value)
+
+	def set_value(self, value):
+		self.input_data.append(value)
+		
+		self.count+=1
 	
+	def set_mean(self, value):
+		self.mean_data.append(value)
+		
+		self.mean_value_count+=1
+		
+	def isFull(self):
+		if self.mean_value_count == 200:
+			self.l_input_data.append(self.mean_data)
+			self.mean_data = array.array('i')
+			self.mean_value_count = 0
+		if self.count == 200:
+			self.l_input_data.append(self.input_data)
+			self.input_data = array.array('i')
+			self.count = 0
+
 def inclination(x_range, y_range):
 	value = y_range/x_range
 	return value
@@ -87,32 +94,28 @@ def mean_values(new_value, previous_value):
 	new_sum = new_value + previous_value
 	near_sum = new_sum/2
 	return near_sum
-	
-	
+
+
 def elapsed(start_time):
 	now = time.time()
-	now_milli = convert_millis(now)
+	between = now - start_time
+	'''now_milli = convert_millis(now)
 	start_milli = convert_millis(start_time)
 	between_milli = now_milli - start_milli
+	millis = between_milli'''
 	#print(now_milli, start_milli, between_milli)
-	millis = between_milli
+	millis = convert_millis(between)
 	return millis
 
 def convert_millis(seconds):
 	milliseconds = round(seconds*1000)
 	return milliseconds
 
-def smooth_data(stored_values):
-	new_value = stored_values['current_inclination']
-	previous_value = stored_values['previous_inclination']
-	
-	mean_value = mean_values(new_value, previous_value)
-	
-	return mean_value
+
 
 def makeplot(x, y):
 	plt.plot(x, y)
-	
+
 	plt.xlabel('X Range')
 	plt.ylabel('Y Range')
 	plt.title('Plot of X Range and Y Range')
@@ -122,5 +125,5 @@ def makeplot(x, y):
 if __name__ == '__main__':
 	file_path = "C:\\Users\\kiho\\OneDrive\\デスクトップ\\morse\\KihoData.txt"
 	file = open(file_path, 'r')
-	main(file)
+	morse(file).main()
 	
